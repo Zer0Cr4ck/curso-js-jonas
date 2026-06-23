a2464243aaebbb04dda98a1b69795b8fe3a84442
Zer0Cr4ck <jlt_xd@hotmail.com>
2026-06-22 19:40:25 -0500

    Add files via upload

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,28 @@
+let js = "amazing";
+// if (js === "amazing") alert("Js is fun");
+console.log(typeof (40 + 8 + 23 - 10));
+
+// let joel = "name";
+
+sport = "skate";
+console.log(typeof sport);
+
+const firstName = "Joel";
+const job = "teacher";
+const birthYear = 1992;
+const year = 2037;
+
+const joel =
+  "I'm " + firstName + ", a " + (year - birthYear) + " years old " + job + "!";
+
+console.log(joel);
+
+//PRACTICA PERSONAL GEMINI
+
+const melamina = "pelikano";
+let precioPlancha = 250;
+let cantidadMelamina = 3;
+
+let costoTotal = precioPlancha * cantidadMelamina;
+
+costoTotal > 500 ? console.log(`Gama Alta`) : console.log(`Gama Estandar`);
